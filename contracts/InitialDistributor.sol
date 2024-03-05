8b754a2f049ffcbdfe53659f0dc1b0c5e4a621a8
Webber-Woo <95848388+Webber-Woo@users.noreply.github.com>
2024-03-05 16:08:03 +0800

    Create InitialDistributor.sol

---
+++ contracts/InitialDistributor.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./interfaces/IYaka.sol";
+import "./interfaces/IVotingEscrow.sol";
+import "./interfaces/IInitialDistributor.sol";
+import "./Minter.sol";
+
+contract InitialDistributor is IInitialDistributor {
+
+}
+
+
+    
