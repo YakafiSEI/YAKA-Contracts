0fd84bb659e0466e020b4d437d8c5446c8a77c1c
zhgsnake <zhgsnake@163.com>
2024-03-08 17:05:47 +0800

    Update ERC20Token.sol

--- contracts/test/ERC20Token.sol
+++ contracts/test/ERC20Token.sol
@@ -11,4 +11,10 @@ contract ERC20Token is ERC20 {
         _mint(msg.sender, 1000*1e18);
     }
 
+    function mint(address account, uint256 amount) external returns (bool) {
+        _mint(account, amount);
+        return true;
+    }
+
+
 }
