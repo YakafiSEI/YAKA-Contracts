70baffc882de28e25fce412c9a8ea36dc5b7767e
Webber-Woo <95848388+Webber-Woo@users.noreply.github.com>
2024-04-23 17:25:07 +0800

    Update EpochController.sol

--- contracts/chainlink/EpochController.sol
+++ contracts/chainlink/EpochController.sol
@@ -4,37 +4,79 @@ pragma solidity ^0.8.7;
 import "../chainlink/AutomationCompatible.sol";
 import "../interfaces/IMinter.sol";
 import "../interfaces/IVoter.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-contract EpochController is AutomationCompatibleInterface, OwnableUpgradeable  {
+contract EpochController is AutomationCompatibleInterface, Ownable  {
 
     address public automationRegistry;
 
     address public minter;
     address public voter;
 
+    
+    // last time automation was called
+    uint256 public lastCalledAt;
+    // last time automation completed all distribution
+    uint256 public lastCompletedAt;
 
-    constructor() {}
+    // track pool length
+    uint256 public index;
+    // max number of gauges for batch (used to limit gas on each call)
+    uint256 public maxLoops;
 
-    function initialize() public initializer {
-        __Ownable_init();
+    uint256 constant public WEEK = 7 * 86400;
+
+
+    constructor() {
         minter = address(0x86069FEb223EE303085a1A505892c9D4BdBEE996);
-        voter = address(0x62Ee96e6365ab515Ec647C065c2707d1122d7b26);
+        voter = address(0x3A1D0952809F4948d15EBCe8d345962A282C4fCb);
         automationRegistry = address(0x02777053d6764996e594c3E88AF1D58D5363a2e6);
+        maxLoops = 10;
+        lastCalledAt = block.timestamp;
+        lastCompletedAt = block.timestamp / WEEK * WEEK;
     }
 
 
     function checkUpkeep(bytes memory /*checkdata*/) public view override returns (bool upkeepNeeded, bytes memory /*performData*/) {
-        upkeepNeeded = IMinter(minter).check();
+        upkeepNeeded = IMinter(minter).check() || lastCompletedAt + WEEK <= block.timestamp;
     }
 
     function performUpkeep(bytes calldata /*performData*/) external override {
         require(msg.sender == automationRegistry || msg.sender == owner(), 'cannot execute');
-        (bool upkeepNeeded, ) = checkUpkeep('0');
+        bool upkeepNeeded = IMinter(minter).check() || lastCompletedAt + WEEK <= block.timestamp;
         require(upkeepNeeded, "condition not met");
-        IVoter(voter).distributeAll();
+
+        uint length = IVoter(voter).length() - 1;
+        uint end = index + maxLoops - 1;
+
+        if(end > length) end = length;
+
+        IVoter(voter).distribute(index, end);    
+        
+        index += maxLoops;       
+        if(index >= length) {
+            lastCalledAt = block.timestamp;
+            lastCompletedAt = block.timestamp / WEEK * WEEK;
+            index = 0;
+        } 
     }
+
+
+
+
+
+    function setIndex(uint _index) external onlyOwner {
+        index = _index;
+    }
+
+
+
+    function setMaxLoops(uint _maxLoops) external onlyOwner {
+        maxLoops = _maxLoops;
+    }
+
+
 
     function setAutomationRegistry(address _automationRegistry) external onlyOwner {
         require(_automationRegistry != address(0));
