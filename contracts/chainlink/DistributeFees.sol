b11330b84bef50fa80f14131cc1c8518e310dfbb
Webber-Woo <95848388+Webber-Woo@users.noreply.github.com>
2024-04-23 11:26:24 +0800

    Update DistributeFees.sol

--- contracts/chainlink/DistributeFees.sol
+++ contracts/chainlink/DistributeFees.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.20;
 
 import "./AutomationCompatibleInterface.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
