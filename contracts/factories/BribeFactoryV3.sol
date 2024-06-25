2d38f50aabbf8bfab14413172dd674d4655c7e42
Webber-Woo <webber.wyw@gmail.com>
2024-06-25 11:26:35 +0800

    Ibribe接口增加addRewardToken用于BribeFactoryV3外部调用

--- contracts/factories/BribeFactoryV3.sol
+++ contracts/factories/BribeFactoryV3.sol
@@ -8,6 +8,7 @@ import '../interfaces/IPermissionsRegistry.sol';
 
 interface IBribe {
     function addReward(address) external;
+    function addRewardToken(address _rewardsToken) external;
     function setVoter(address _Voter) external;
     function setMinter(address _Voter) external;
     function setOwner(address _Voter) external;
@@ -121,14 +122,14 @@ contract BribeFactoryV3 is OwnableUpgradeable {
 
     /// @notice Add a reward token to a given bribe
     function addRewardToBribe(address _token, address __bribe) external onlyAllowed {
-        IBribe(__bribe).addReward(_token);
+        IBribe(__bribe).addRewardToken(_token);
     }
 
     /// @notice Add multiple reward token to a given bribe
     function addRewardsToBribe(address[] memory _token, address __bribe) external onlyAllowed {
         uint i = 0;
         for ( i ; i < _token.length; i++){
-            IBribe(__bribe).addReward(_token[i]);
+            IBribe(__bribe).addRewardToken(_token[i]);
         }
     }
 
@@ -136,7 +137,7 @@ contract BribeFactoryV3 is OwnableUpgradeable {
     function addRewardToBribes(address _token, address[] memory __bribes) external onlyAllowed {
         uint i = 0;
         for ( i ; i < __bribes.length; i++){
-            IBribe(__bribes[i]).addReward(_token);
+            IBribe(__bribes[i]).addRewardToken(_token);
         }
 
     }
@@ -148,7 +149,7 @@ contract BribeFactoryV3 is OwnableUpgradeable {
         for ( i ; i < __bribes.length; i++){
             address _br = __bribes[i];
             for(k = 0; k < _token.length; k++){
-                IBribe(_br).addReward(_token[i][k]);
+                IBribe(_br).addRewardToken(_token[i][k]);
             }
         }
 
